bf56c0f475257e37e7ea7da44fe703f47268b82d
Abhishek Jain <abhishek.jain@trili.tech>
2023-05-24 15:17:55 +0100

    push broken code

---
+++ .storybook/main.js
@@ -0,0 +1,6 @@
+module.exports = {
+  addons: ['@chakra-ui/storybook-addon'],
+  features: {
+    emotionAlias: false,
+  },
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,43 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  ChakraProvider,
+  Flex,
+  HStack,
+  IconButton,
+  useBreakpointValue
+} from '@chakra-ui/react'
+import { Logo } from './Logo'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export const App = () => {
+  const isDesktop = useBreakpointValue({ base: false, lg: true })
+  return (
+    <Box as="section" pb={{ base: '12', md: '24' }}>
+      <Box as="nav" bg="bg-surface" boxShadow="sm">
+        <Container py={{ base: '4', lg: '5' }}>
+          <HStack spacing="10" justify="space-between">
+            <Logo />
+            {isDesktop ? (
+              <Flex justify="space-between" flex="1">
+                <HStack spacing="3">
+                  <Button variant="primary">Connect</Button>
+                </HStack>
+              </Flex>
+            ) : (
+              <IconButton
+                variant="ghost"
+                aria-label="Open Menu"
+              />
+            )}
+          </HStack>
+        </Container>
+      </Box>
+    </Box>
+  )
 }
+
+export default App
