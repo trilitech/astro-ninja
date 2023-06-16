0dda1e73c5952b68a9dc870b5600c0c5f07b5c8a
bigbluebroccoli <bigbluebroccoli@gmail.com>
2023-06-16 17:03:33 +0100

    reversed some additional features that are already in PR 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,25 +49,19 @@ const NavBar = () => {
   };
 
   return (
-    <Box as="section">
+    <Box as="section" pb={{ base: "12", md: "24" }}>
       <Box as="nav" bg="bg-surface" boxShadow="sm">
         <Box px="48px" py={{ base: "4", lg: "5" }}>
           <HStack spacing="10" justify="space-between">
-            <Text fontWeight="bold" fontSize="1.2rem" onClick={gotohome}>
-              ✨AstroNinja
-            </Text>
+            <Text onClick={gotohome}>AstroNinja</Text>
             {address ? (
-              <Avatar address={address} />
+              <>
+                <Avatar address={address} />{" "}
+              </>
             ) : (
               <Flex justify="right" flex="1">
                 <HStack spacing="10">
-                  <Button
-                    className="btn"
-                    variant="primary"
-                    onClick={connect}
-                    borderRadius="full"
-                    fontWeight="700"
-                  >
+                  <Button variant="primary" onClick={connect}>
                     Connect
                   </Button>
                 </HStack>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,13 @@
 import { Inter } from "next/font/google";
-import {
-  Box,
-  Button,
-  Image,
-  Flex,
-  Text,
-  useBreakpointValue,
-} from "@chakra-ui/react";
+import { Box, Button, Image, Flex } from "@chakra-ui/react";
 import { useConnection } from "@/packages/providers";
 import { useRouter } from "next/router";
-import Footer from "../components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { address, connect } = useConnection();
   const router = useRouter();
-  const displayMobile = useBreakpointValue({ base: true, md: false });
-
   const claim = () => {
     if (!address) {
       connect();
@@ -29,64 +19,27 @@ export default function Home() {
   return (
     <>
       <main>
-        <Flex mt={50} mb={50} direction={displayMobile ? "column" : "row"} className="hero">
+        <Flex width="100%" height="400px">
           <Flex
             flex={1}
             padding="10px"
             direction="column"
             justifyContent="center"
           >
-            <Flex alignItems="center">
-              <Box
-                boxSize="40px"
-                borderRadius="full"
-                border="1px"
-                overflow="hidden"
-                mr={2}
-              >
-                <Image src="/contractimg.png" alt="Contract Profile Image" />
-              </Box>
-              <Text fontWeight="bold">AstroNinja</Text>
-            </Flex>
-            <Text fontSize="4xl" fontWeight="bold" mb="2vh">
-              AstroNinja NFT
-            </Text>
-            <Text fontSize="3xl" mb="2vh">
-              1.00 ꜩ
-            </Text>
-            <Text maxWidth="380px" fontSize="medium" mb="4vh">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              sed lacus eget est consectetur ornare. Praesent imperdiet porta
-              ligula a dapibus...
-            </Text>
+            <p>
+              Astronaut Ninja NFT Collection, Click here to claim your
+              collectible
+            </p>
             <Box>
-              <Button
-                className="btn"
-                width="50%"
-                variant="primary"
-                borderRadius="full"
-                onClick={claim}
-                fontWeight="700"
-              >
-                Buy now
+              <Button colorScheme="blue" onClick={claim}>
+                Claim
               </Button>
             </Box>
           </Flex>
-          {!displayMobile && (
-            <Image src="/astroninja.png" alt="Astro Ninja" width="45%" />
-          )}
-          {displayMobile && (
-            <Image
-              src="/astroninja.png"
-              alt="Astro Ninja"
-              height="auto"
-              maxH="75vh"
-              alignSelf="center"
-              mt="15px"
-            />
-          )}
+          <Box flex={1} padding="10px">
+            <Image src="/astroninja.png" width={360} height={360} />
+          </Box>
         </Flex>
-        <Footer />
       </main>
     </>
   );
